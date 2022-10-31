44772c03e9df3ad4c4b0a9b13e46133b2034dd85
Beavis <chris.e.ewing@gmail.com>
2022-10-31 10:25:52 -0600

    database fixes

--- resources/migrations/91.sql
+++ resources/migrations/91.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `instance`
-	CHANGE COLUMN `type` `type` ENUM('circle_pokemon','circle_smart_pokemon','circle_raid','circle_smart_raid','auto_quest','pokemon_iv','leveling','circle_jumpy_pokemon') NOT NULL COLLATE 'utf8mb4_general_ci' AFTER `name`;
+	CHANGE COLUMN `type` `type` ENUM('circle_pokemon','circle_smart_pokemon','circle_raid','circle_smart_raid','auto_quest','pokemon_iv','leveling','jumpy_pokemon','findy_pokemon') NOT NULL COLLATE 'utf8mb4_general_ci' AFTER `name`;
