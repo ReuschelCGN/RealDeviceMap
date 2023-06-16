488a976e8707474e5ff3ff4fc1f47bfd75e50906
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2023-06-16 13:16:33 +0200

    Update Account.swift

--- Sources/RealDeviceMapLib/Modell/Account.swift
+++ Sources/RealDeviceMapLib/Modell/Account.swift
@@ -609,7 +609,7 @@ public class Account: WebHookEvent {
                 \(spinSQL)
                 \(cooldownSQL)
                 \(lockoutSQL)
-            ORDER BY rand()
+            ORDER BY level DESC, last_used_timestamp ASC
             LIMIT 1
         """
 
